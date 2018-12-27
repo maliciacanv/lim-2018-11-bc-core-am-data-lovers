958795f810f5266e9d6d998e86d2ebd2694c3a67
maliciacanv <cantenovictoriomaruja@gmail.com>
2018-12-27 14:45:08 -0500

    html del segundo historia de usuario

--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+// primer historia de usuario pide mostrar los 10 mejores campeones y medido por su ataque de rango//
 const championsList = (arr) => {
   const newArray = [];
   for (let i = 0; i < arr.length; i++) {
-    newArray.push({name: arr[i].name, title: arr[i].title, img: arr[i].img, attackrange: arr[i].stats.attackrange});
+    newArray.push({ name: arr[i].name, title: arr[i].title, splash: arr[i].splash, attackrange: arr[i].stats.attackrange, tags: arr[i].tags });
   }
   const newArrayOrder = newArray.sort((higher, less) => {
     if (higher.attackrange < less.attackrange) {
@@ -15,8 +14,25 @@ const championsList = (arr) => {
   });
   return newArrayOrder.slice(0, 10);
 };
+// segunda historia de usuario pide filtrar los campeones por roles: asesino,mago,tanque//
+const championsPerRoles = (arr) => {
+  let newArrayRol = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArrayRol.push({ tags: arr[i].tags, name: arr[i].name, splash: arr[i].splash });
+  }
+  const yy = newArrayRol.filter(ele => {
+    if (ele.tags.includes('Assassin')) {
+      return true;
+    }
+    else if (ele.tags.includes('Fighter')) {
+      return true;
+    }
+  });
+  console.log(yy);
+};
 
 window.lol = {
   championsList,
+  championsPerRoles,
 };
 
--- src/main.js
+++ src/main.js
@@ -1,22 +1,34 @@
 // conviertiendo la data objeto en array//
 const arrDataLolTotal = Object.values(LOL.data);
-const containerListDatosHTML = document.getElementById('container-listimagens');
-// Llamar a mi funcion listarPersonajes -> name, title,img, atackrange//
+const containerListDatosHTML = document.getElementById('container-listImagens');
+// Llamar a mi funcion listarPersonajes -> name, title, img, atackrange//
 const arrDataLolPrecisa = lol.championsList(arrDataLolTotal);
-const firstUser = document.getElementById('topon');
+const firstUser = document.getElementById('topteam');
 firstUser.addEventListener('click', () => {
   for (let i = 0; i < arrDataLolPrecisa.length; i++) {
     containerListDatosHTML.innerHTML +=
-          `<div class="card-link">
-            <article class="blog-card">
-              <img class="post-image" src="${arrDataLolPrecisa[i].img}"/>
-              <div class="list-data">
+      `<div class="card-link">
+              <img class="post-image" src="${arrDataLolPrecisa[i].splash}"/>
               <li class="post-name">${arrDataLolPrecisa[i].name}</li>
-               <li class="post-title"> ${arrDataLolPrecisa[i].title}</li>
-               <li class="post-attackrange">
-               Attackrange: ${arrDataLolPrecisa[i].attackrange}</li>
-             </div>
-           </article>
-          </div>`;
+              <li class="post-title"> ${arrDataLolPrecisa[i].title}</li>
+              <li class="post-attackrange">Attackrange: ${arrDataLolPrecisa[i].attackrange}</li>
+       </div>`;
   }
 });
+console.log(containerListDatosHTML);
+
+
+const containerListRolesHTML = document.getElementById('container-listRoles');
+const arrListDeRoles = lol.championsPerRoles(arrDataLolTotal);
+const firstUserRol = document.getElementById('assassin');
+firstUserRol.addEventListener('click', () => {
+  for (let i = 0; i < arrListDeRoles.length; i++) {
+    containerListRolesHTML.innerHTML +=
+      `<div class ="card-list">
+          <img class ="image" src = "${arrListDeRoles[i].splash}"/>
+          <li class ="name"> ${arrListDeRoles[i].name}</li>
+        </div>`;
+  }
+});
+console.log(containerListRolesHTML);
+console.log(firstUserRol);
