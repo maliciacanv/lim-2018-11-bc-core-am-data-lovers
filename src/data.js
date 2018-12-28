52aa95b3bfaaa646e18aec7e823d94ebfea04fcf
aurislucero <luro.corilla@gmail.com>
2018-12-28 16:25:07 -0500

    filter in the second user data.js

--- src/data.js
+++ src/data.js
@@ -1,8 +1,31 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
+const championsList = (arr) => {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push({ name: arr[i].name, title: arr[i].title, splash: arr[i].splash, attackrange: arr[i].stats.attackrange });
+  }
+  const newArrayOrder = newArray.sort((higher, less) => {
+    if (higher.attackrange < less.attackrange) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return newArrayOrder.slice(0, 10);
+};
+const filterData = (data, type) => {
+ const filterNewData = data.map(importData => Object.assign({}, {name: importData.name,splash: importData.splash,tags:importData.tags}));
 
-const example = () => {
-  return 'example';
+  const filterRoles = filterNewData.filter((data) => {
+    return data.tags.includes(type) === true;
+  });
+  return filterRoles;
 };
 
-window.example = example;
+window.lol = {
+
+  championsList,
+  filterData,
+};
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
-const champions=Object.values(LOL.data);
-const obtenerPropiedad = (data) => {
-    let filterNew = [];
-    for (let i=0; i < data.length ;i++){
-      filterNew.push(data[i].tags);
-     // filterNew.push(data[i].name);
+// conviertiendo la data objeto en array//
+// eslint-disable-next-line no-undef
+const arrDataLolTotal = Object.values(LOL.data);
+const containerListDatosHTML = document.getElementById('container-listimagens');
+
+
+const arrDataLolPrecisa = lol.championsList(arrDataLolTotal);
+const firstUser = document.getElementById('topon');
+
+
+
+firstUser.addEventListener('click', () => {
+  for (let i = 0; i < arrDataLolPrecisa.length; i++) {
+    containerListDatosHTML.innerHTML +=
+      `<div class="card-link">
+            <article class="blog-card">
+              <img class="post-image" src="${arrDataLolPrecisa[i].splash}"/>
+              <div class="list-data">
+              <li class="post-name">${arrDataLolPrecisa[i].name}</li>
+               <li class="post-title"> ${arrDataLolPrecisa[i].title}</li>
+               <li class="post-attackrange">
+               Attackrange: ${arrDataLolPrecisa[i].attackrange}</li>
+             </div>
+           </article>
+          </div>`;
   }
-  return filterNew;
-  }
-  document.write(obtenerPropiedad(champions));
+});
+const type = 'Tank';
+const arrDataRole = lol.filterData(arrDataLolTotal, type);
+
+console.log(arrDataRole);
+
+
